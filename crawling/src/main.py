9dd50abe95f0090ce61b0b0bec1060591f1a4ae7
nqqduy <duynguyen1332000@gmail.com>
2023-06-12 22:55:28 +0700

    crawling done

--- crawling/src/main.py
+++ crawling/src/main.py
@@ -76,8 +76,8 @@ class Main(webdriver.Chrome):
             self.cursor.execute(create_table_query)
             
     def initalize_database(self):
-        create_table_races_query = """CREATE TABLE races (id INT NOT NULL AUTO_INCREMENT, pos VARCHAR(255),  no VARCHAR(255), driver VARCHAR(255), car VARCHAR(255), laps VARCHAR(255), retired VARCHAR(255), pts INT, date VARCHAR(255), location VARCHAR(255), location_key VARCHAR(255), year VARCHAR(255), PRIMARY KEY (id));"""
-        self.check_exists_table('races', create_table_races_query)
+        create_table_races_query = """CREATE TABLE result (id INT NOT NULL AUTO_INCREMENT, pos VARCHAR(255),  no VARCHAR(255), driver VARCHAR(255), nationality VARCHAR(255), car VARCHAR(255), laps VARCHAR(255), retired VARCHAR(255), pts INT, date VARCHAR(255), location VARCHAR(255), location_key VARCHAR(255), year VARCHAR(255), PRIMARY KEY (id));"""
+        self.check_exists_table('result', create_table_races_query)
 
         # todo
 
@@ -125,20 +125,27 @@ class Main(webdriver.Chrome):
                         category_link, category_key, category_value = category_tuple
                         category_url_for_crawl =  self.racing_host + category_link
                 
-                        if category_key in [RACES, DRIVERS, TEAMS]:
+                        if category_key in [RACES, DRIVERS]:
                             self.get(category_url_for_crawl)
 
                             detail_list = self.get_total_url_and_data_value_on_filter(POSTION_2_ON_FILTER)
                             if len(detail_list) > 0:
-                                specific_detail_list = detail_list[1:] # discard all value
-                                for specific_detail_tuple in specific_detail_list:
+                                if category_key == RACES:
+                                    specific_detail_list = detail_list[1:] # discard all value
+                                    for specific_detail_tuple in specific_detail_list:
+                                        detail_link, link_key, detail_value = specific_detail_tuple
+                                        detail_url_for_crawl =  self.racing_host + detail_link
+                                        self.get(detail_url_for_crawl)
+
+                                        self.crawl_result_racing(category_key, link_key, detail_value, year_key)
+                                elif category_key == DRIVERS:
+                                    specific_detail_tuple = detail_list[0]
                                     detail_link, link_key, detail_value = specific_detail_tuple
-                            
                                     detail_url_for_crawl =  self.racing_host + detail_link
-
                                     self.get(detail_url_for_crawl)
-
                                     self.crawl_result_racing(category_key, link_key, detail_value, year_key)
+
+
         else: 
             print('No data for crawling')
 
@@ -153,51 +160,14 @@ class Main(webdriver.Chrome):
 
                 self.upsert_races_into_database(df)
         elif category_key == DRIVERS:
-            pass
-        elif category_key == TEAMS:
-            pass
+            df = self.crawl_result_racing_with_driver_table()
+            if df:
+                df = pd.DataFrame(df)
+                df['year'] = year_value
+
+                self.update_drivers_into_database(df)
     
     def crawl_result_racing_with_races_table(self):
-        drivers_df =  copy.deepcopy(define.races_df) 
-        html = self.page_source
-        soup = BeautifulSoup(html, "html.parser")
-
-        table_element = soup.find('table', 'resultsarchive-table')
-        if table_element:
-            date = soup.find('div', 'resultsarchive-content-header').find('p', "date").find('span', 'full-date').text.strip()
-            tbody_element = table_element.find('tbody')
-
-            rows = tbody_element.find_all('tr')
-
-            for row in rows:
-                th_element = row.find_all('td')
-
-                pos = th_element[1].text.strip()
-                no = th_element[2].text.strip()
-
-                driver_elements = th_element[3].find_all('span')
-                driver = driver_elements[0].text.strip() + " " + driver_elements[1].text.strip() 
-
-                car = th_element[4].text.strip()
-                laps = th_element[5].text.strip()
-
-                retired = th_element[6].text.strip()
-
-                pts = th_element[7].text.strip()
-
-                races_df['pos'].append(pos)
-                races_df['no'].append(no)
-                races_df['driver'].append(driver)
-                races_df['car'].append(car)
-                races_df['laps'].append(laps)
-                races_df['retired'].append(retired)
-                races_df['pts'].append(pts)
-                races_df['date'].append(date)
-
-            return races_df
-        return None
-    
-    def crawl_result_racing_with_driver_table(self):
         races_df =  copy.deepcopy(define.races_df) 
         html = self.page_source
         soup = BeautifulSoup(html, "html.parser")
@@ -237,6 +207,30 @@ class Main(webdriver.Chrome):
             return races_df
         return None
     
+    def crawl_result_racing_with_driver_table(self):
+        driver_df =  copy.deepcopy(define.drivers_df) 
+        html = self.page_source
+        soup = BeautifulSoup(html, "html.parser")
+
+        table_element = soup.find('table', 'resultsarchive-table')
+        if table_element:
+            tbody_element = table_element.find('tbody')
+
+            rows = tbody_element.find_all('tr')
+
+            for row in rows:
+                th_element = row.find_all('td')
+                driver_elements = th_element[2].find_all('span')
+                driver = driver_elements[0].text.strip() + " " + driver_elements[1].text.strip()    
+
+                nationality = th_element[3].text.strip()
+
+                driver_df['driver'].append(driver)
+                driver_df['nationality'].append(nationality)
+             
+            return driver_df
+        return None
+    
     def upsert_races_into_database(self, df):
         for index, row in df.iterrows():
             pos = row['pos']
@@ -251,16 +245,33 @@ class Main(webdriver.Chrome):
             location_key = row['location_key']
             year = row['year']
 
-            check_row_exists_query = f"SELECT id FROM races WHERE driver = '{driver}' AND location_key = '{location_key}' AND year = '{year}'"
+            check_row_exists_query = f"SELECT id FROM result WHERE driver = '{driver}' AND location_key = '{location_key}' AND year = '{year}'"
             db_data = pd.read_sql(check_row_exists_query, con=self.engine)
             
             if len(db_data) > 0:
                 id = db_data['id'].values[0]
-                update_query = f"UPDATE races SET pos = '{pos}', no = '{no}', car = '{car}', laps = '{laps}', retired = '{retired}', pts = '{pts}', date = '{date}' WHERE id = {id}" 
+                update_query = f"UPDATE result SET pos = '{pos}', no = '{no}', car = '{car}', laps = '{laps}', retired = '{retired}', pts = '{pts}', date = '{date}' WHERE id = {id}" 
 
                 self.cursor.execute(update_query)
             else:
-                insert_query = f"INSERT INTO races (pos, no, car, laps, retired, pts, date, driver, location, location_key, year) VALUES ('{pos}', '{no}', '{car}', '{laps}', '{retired}', '{pts}', '{date}', '{driver}', '{location}', '{location_key}', '{year}')"
+                insert_query = f"INSERT INTO result (pos, no, car, laps, retired, pts, date, driver, location, location_key, year) VALUES ('{pos}', '{no}', '{car}', '{laps}', '{retired}', '{pts}', '{date}', '{driver}', '{location}', '{location_key}', '{year}')"
                 
                 self.cursor.execute(insert_query)
-        self.cursor.execute("commit;")
+        self.cursor.execute("commit;") 
+    
+    def update_drivers_into_database(self, df):
+        for index, row in df.iterrows():
+            driver = row['driver']
+            nationality = row['nationality']
+            year = row['year']
+
+            check_row_exists_query = f"SELECT id FROM result WHERE driver = '{driver}' AND year = '{year}'"
+            db_data = pd.read_sql(check_row_exists_query, con=self.engine)
+
+            if len(db_data) > 0:
+                id = db_data['id'].values[0]
+                update_query = f"UPDATE result SET nationality = '{nationality}' WHERE driver = '{driver}' AND year = '{year}'" 
+
+                self.cursor.execute(update_query)
+        self.cursor.execute("commit;") 
+
